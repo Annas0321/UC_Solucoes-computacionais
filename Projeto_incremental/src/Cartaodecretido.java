a97d63644ffbb5e77d31cdd83547975e63251ca0
AnnaSouza <164109781+Annas0321@users.noreply.github.com>
2024-10-31 22:08:45 -0300

    Incremento 3

--- Projeto_incremental/src/Cartaodecretido.java
+++ Projeto_incremental/src/Cartaodecretido.java
@@ -1,10 +1,32 @@
 public class Cartaodecretido {
     private int numero;
-    private String nomeTitular; 
-    private String cpf;
+    private String nomeTitular;
+    private String cpf; 
     private double limite;
     private double totalFatura;
+    private double taxaCashback; 
 
+    
+    public Cartaodecretido(int numero, String nomeTitular, String cpf) {
+        this.numero = numero;
+        this.nomeTitular = nomeTitular;
+        this.cpf = cpf; 
+        this.limite = 1000; 
+        this.totalFatura = 0;
+        this.taxaCashback = 0;
+    }
+
+   
+    public Cartaodecretido(int numero, String nomeTitular, String cpf, double limite, double taxaCashback) {
+        this.numero = numero;
+        this.nomeTitular = nomeTitular;
+        this.cpf = cpf; 
+        this.limite = limite;
+        this.totalFatura = 0;
+        this.taxaCashback = taxaCashback;
+    }
+
+    // Getters e setters
     public int getNumero() {
         return numero;
     }
@@ -12,10 +34,10 @@ public class Cartaodecretido {
     public void setNumero(int numero) {
         this.numero = numero;
     }
-    
+
     public String getNomeTitular() {
         return nomeTitular;
-    } 
+    }
 
     public void setNomeTitular(String nomeTitular) {
         this.nomeTitular = nomeTitular;
@@ -25,7 +47,7 @@ public class Cartaodecretido {
         return cpf;
     }
 
-    public void setCpf(String cpf){
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
@@ -33,35 +55,58 @@ public class Cartaodecretido {
         return limite;
     }
 
-    public void setLimite(double limite){
+    public void setLimite(double limite) {
         this.limite = limite;
     }
 
     public double getTotalFatura() {
-       return totalFatura;
+        return totalFatura;
     }
 
-    public void setTotalFatura(double totalFatura){
+    public void setTotalFatura(double totalFatura) {
         this.totalFatura = totalFatura;
     }
 
-    public double consultarLimite(){
+    public double getTaxaCashback() {
+        return taxaCashback;
+    }
+
+    public void setTaxaCashback(double taxaCashback) {
+        this.taxaCashback = taxaCashback;
+    }
+
+    public double consultarLimite() {
         return getLimite();
     }
 
-    public double consultarTotalFatura(){
+    public double consultarTotalFatura() {
         return getTotalFatura();
     }
 
+    
     public void realizarCompra(double valor) {
-        if (valor <= getLimite()){
+        if (valor <= getLimite()) {
             setLimite(getLimite() - valor);
             setTotalFatura(getTotalFatura() + valor);
             System.out.println("Compra realizada com sucesso!");
-        } 
-        else{
-            System.out.println("Você não possui limite necessário para essa compra");
+        } else {
+            System.out.println("Você não possui limite necessário para essa compra.");
         }
     }
 
+   
+    public void realizarCompra(double valor, boolean aplicarCashback) {
+        if (aplicarCashback && taxaCashback > 0) {
+            if (valor <= getLimite()) {
+                double cashback = valor * (taxaCashback / 100);
+                setLimite(getLimite() - valor + cashback);
+                setTotalFatura(getTotalFatura() + valor - cashback);
+                System.out.println("Compra com cashback realizada com sucesso! Cashback: R$" + cashback);
+            } else {
+                System.out.println("Você não possui limite necessário para essa compra.");
+            }
+        } else {
+            realizarCompra(valor); 
+        }
+    }
 }
--- Projeto_incremental/src/Principal.java
+++ Projeto_incremental/src/Principal.java
@@ -2,41 +2,75 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Cartaodecretido c = new Cartaodecretido();
-        c.setCpf("12312352636");
-        c.setLimite(100);
         Scanner sc = new Scanner(System.in);
+        Cartaodecretido c;
+
+        System.out.println("Bem-vindo ao sistema de criação de cartão de crédito!");
+        System.out.println("Escolha uma opção para criar o cartão:");
+        System.out.println("1 - Criar cartão com informações básicas");
+        System.out.println("2 - Criar cartão com limite personalizado e cashback");
+
+        int escolha = sc.nextInt();
+
+        System.out.println("Digite o número do cartão:");
+        int numero = sc.nextInt();
+        sc.nextLine();  
+
+        System.out.println("Digite o nome do titular:");
+        String nomeTitular = sc.nextLine();
+
+        System.out.println("Digite o CPF do titular:");
+        String cpf = sc.nextLine(); // Coletando o CPF
+
+        if (escolha == 1) {
+            c = new Cartaodecretido(numero, nomeTitular, cpf); 
+        } else {
+            System.out.println("Digite o limite de crédito:");
+            double limite = sc.nextDouble();
+
+            System.out.println("Digite a taxa de cashback (%):");
+            double taxaCashback = sc.nextDouble();
+
+            c = new Cartaodecretido(numero, nomeTitular, cpf, limite, taxaCashback); 
+        }
 
         int opcao;
-
         do {
             System.out.println("1 - Consultar Limite");
             System.out.println("2 - Consultar Fatura");
-            System.out.println("3 - Realizar compra");
+            System.out.println("3 - Realizar compra básica");
+            System.out.println("4 - Realizar compra com cashback");
             System.out.println("0 - Sair");
 
             opcao = sc.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.println(c.consultarLimite());
-                     break;
+                    System.out.println("Limite disponível: " + c.consultarLimite());
+                    break;
                 case 2:
-                    System.out.println(c.consultarTotalFatura());
+                    System.out.println("Total fatura: " + c.consultarTotalFatura());
                     break;
                 case 3:
-                    System.out.println("Digite o valor da compra");
+                    System.out.println("Digite o valor da compra:");
                     double valor = sc.nextDouble();
                     c.realizarCompra(valor);
+                    break;
+                case 4:
+                    System.out.println("Digite o valor da compra:");
+                    valor = sc.nextDouble();
+                    c.realizarCompra(valor, true);
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema...");
                     break;
                 default:
                     System.out.println("Opção inválida");
                     break;
-
             }
-     
+
         } while (opcao != 0);
-        
+
         sc.close();
     }
 }
